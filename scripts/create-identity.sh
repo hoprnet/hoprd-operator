7c7f0d73ea3948eac6df0081daef3675591bae56
ausias-armesto <ausiasarmesto@gmail.com>
2023-11-01 14:07:25 +0100

    Add contracts dir

--- scripts/create-identity.sh
+++ scripts/create-identity.sh
@@ -8,4 +8,4 @@
 
     set -x
     /bin/hopli identity --action create --identity-directory /app/hoprd-identity-created --identity-prefix .hoprd | tee /app/hoprd-identity-created/hoprd.json
-    /bin/hopli create-safe-module --network "${HOPRD_NETWORK}" --identity-from-path /app/hoprd-identity-created/.hoprd0.id --hopr-amount "10" --native-amount "0.01" | tee "/app/hoprd-identity-created/create-safe-module.log"
+    /bin/hopli create-safe-module --contracts-root /root/contracts --network "${HOPRD_NETWORK}" --identity-from-path /app/hoprd-identity-created/.hoprd0.id --hopr-amount "10" --native-amount "0.01" | tee "/app/hoprd-identity-created/create-safe-module.log"
