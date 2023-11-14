8decdb07d8f37ab7a924a144373d4f61c50d2f8e
ausias-armesto <ausiasarmesto@gmail.com>
2023-11-14 14:27:10 +0100

    Change branch

--- src/constants.rs
+++ src/constants.rs
@@ -3,8 +3,7 @@ pub const RECONCILE_FREQUENCY: u64 = 10;
 pub const OPERATOR_ENVIRONMENT: &str = "OPERATOR_ENVIRONMENT";
 pub const OPERATOR_FINALIZER: &str = "hoprds.hoprnet.org/finalizer";
 pub const OPERATOR_JOB_TIMEOUT: u64 = 300;
-pub const OPERATOR_JOB_SCRIPT_URL: &str =
-    "https://raw.githubusercontent.com/hoprnet/hoprd-operator/ausias/refactor-operator/scripts";
+pub const OPERATOR_JOB_SCRIPT_URL: &str ="https://raw.githubusercontent.com/hoprnet/hoprd-operator/master/scripts";
 // This value `OPERATOR_NODE_SYNC_TIMEOUT` should be lower than 295
 pub const OPERATOR_NODE_SYNC_TIMEOUT: u32 = 290;
 pub const OPERATOR_P2P_MIN_PORT: &str = "9000";
