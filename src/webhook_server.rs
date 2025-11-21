0d4fb492b9f2b97fc3e0cf305116f4136b97ea19
ausias-armesto <ausiasarmesto@gmail.com>
2025-11-21 08:30:56 +0100

    more debug

--- src/webhook_server.rs
+++ src/webhook_server.rs
@@ -5,7 +5,7 @@ use rustls_pemfile::{certs, pkcs8_private_keys};
 use std::{env, io::BufReader, net::SocketAddr};
 use rustls::crypto::ring::default_provider;
 use tracing::{debug, error, info, warn};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de};
 use serde_json::{Value};
 use tokio::net::TcpStream;
 use tokio::time::{sleep, Duration};
@@ -322,6 +322,8 @@ async fn convert_v3_to_v2(resource: &mut Value) -> Result<(), String> {
 
 // Conversion handler
 async fn convert(Json(request): Json<ConversionRequest>) -> impl IntoResponse {
+    debug!("Received conversion request: {:?}", request);
+    debug!("Processing desired_apiversion: {:?}", request.request.desired_apiversion.as_str());
     let mut response_inner = ConversionResponseInner {
         uid: request.request.uid.clone(),
         converted_objects: vec![],
@@ -333,6 +335,7 @@ async fn convert(Json(request): Json<ConversionRequest>) -> impl IntoResponse {
 
     for obj in request.request.objects {
         let mut resource = obj.clone();
+        debug!("Converting resource: {:?}", resource);
 
         let conversion_result = match request.request.desired_apiversion.as_str() {
             "hoprnet.org/v1alpha2" => Ok(convert_v2_to_v3(&mut resource).await),
